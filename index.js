e39d64b6a87c2a608f062e3d4fdaa9fa2dd9234e
Scott Henderson <scott.mc.henderson@gmail.com>
2024-01-29 20:35:33 -0800

    intial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async() => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("http://quotes.toscrape.com/"), {
+        waitUntil: "documentloaded"
+    }
+
+    // Get page data
+    const quotes = await page.evaluate(() => {
+        //Fetch first element with class "quote"
+        const quoteList = document.querySelectorAll(".quote");
+
+        // Convert the quoteList to an iterable array
+        // For each quote fetch the text and author
+
+        return Array.from(quoteList).map((quote) => { 
+            // Fetch sub elments from previously fetched quote element
+            // Get the displayed text and return it ('.innerText')
+            const text = quote.querySelector(".text").innerText;
+            const author = quote.querySelector(".author").innerText;
+
+            return {text, author}
+        })
+    })
+
+    // Display the quotes
+    console.log(quotes)
+
+    // Close the browser
+    await browser.close();
+
+}
+
+// Start the scraping
+getQuotes();
